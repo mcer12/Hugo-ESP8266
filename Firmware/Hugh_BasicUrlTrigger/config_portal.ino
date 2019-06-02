c750c514c680eab7670ac375574f7e92d32662bf
Martin Černý <mail@martincerny.info>
2019-06-02 16:43:03 +0200

    fw number update

--- Firmware/Hugh_BasicUrlTrigger/config_portal.ino
+++ Firmware/Hugh_BasicUrlTrigger/config_portal.ino
@@ -101,7 +101,7 @@ void handleRoot() {
   html += "\"> </div> <div class=\"row\"> <label for=\"b4\">Button 4 url</label> <input type=\"text\" id=\"b4\" name=\"b4\" value=\"";
   html += json["b4"].as<const char*>();
   html += "\"> </div> <div class=\"row\"> <button type=\"submit\">Save and reboot</button> </div> </form> </div>";
-  html += "<div class=\"github\"><p>Basic firmware v1.0, check out <a href=\"https://github.com/mcer12/Hugh-ESP8266\" target=\"_blank\"><strong>Hugh Switch</strong> on GitHub</a></p></div>";
+  html += "<div class=\"github\"><p>Basic firmware v1.1, check out <a href=\"https://github.com/mcer12/Hugh-ESP8266\" target=\"_blank\"><strong>Hugh Switch</strong> on GitHub</a></p></div>";
   html += "</div> </body> </html>";
   server.send(200, "text/html", html);
 

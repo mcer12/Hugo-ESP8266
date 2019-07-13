4d08e4631f59b155e5b315a49f34844c2691253c
Martin Černý <mail@martincerny.info>
2019-07-13 22:00:53 +0200

    initial MQTT sketch by gon, with some changes

---
+++ Firmware/Hugh_MqttTrigger/Hugh_MqttTrigger.ino
@@ -0,0 +1,291 @@
+/*
+  Hugh ESP8266
+  basic mqtt firmware by gon
+  based on the BasicUrlTrigger firmware
+
+  For more information and help, head to Hugh's github repository:
+  https://github.com/mcer12/Hugh-ESP8266
+
+  Do you find Hugh great? Get yourself another one on Tindie:
+  https://www.tindie.com/products/nicethings/hugh-esp8266-4-button-wifi-remote/
+
+  Credits to Marius Motea for his great project. His project was the reason to design
+  the remote in the first place and firmware sketch was initially based on it.
+  https://github.com/diyhue/diyHue
+
+  May contain code lines from the PubSubClient examples
+
+  ***
+
+  MIT License
+
+  Copyright (c) 2019 Martin Cerny
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#include <FS.h>
+#include <ArduinoJson.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+#include <PubSubClient.h>
+
+#include <ArduinoOTA.h>
+#include <Ticker.h>
+
+#define OTA_NAME "Hugh"
+#define AP_NAME "HughConfig"
+#define button1_pin 14
+#define button2_pin 4
+#define button3_pin 12
+#define button4_pin 13
+#define OTA_BLINK_SPEED 100
+#define OTA_TIMEOUT 300000 // 5 minutes
+#define CONFIG_BLINK_SPEED 500
+#define CONFIG_TIMEOUT 300000 // 5 minutes
+// DO NOT CHANGE DEFINES BELOW
+#define NORMAL_MODE 0
+#define OTA_MODE 1
+#define CONFIG_MODE 2
+#define CONFIG_MODE_LOCAL 3
+
+uint8_t deviceMode = NORMAL_MODE;
+
+int button;
+
+int buttonTreshold = 2000;
+
+// BUTTONS TOGGLES
+bool button1toggled = false, button2toggled = false, button3toggled = false, button4toggled = false;
+bool otaModeStarted = false;
+volatile bool ledState = false;
+
+// TIMERS
+unsigned long otaMillis, sleepMillis, ledMillis, configTimer, otaTimer;
+
+int counter;
+byte mac[6];
+
+Ticker ticker;
+ESP8266WebServer server(80);
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+DynamicJsonDocument json(1024); // config buffer
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(16, OUTPUT);
+  pinMode(button1_pin, INPUT);
+  pinMode(button2_pin, INPUT);
+  pinMode(button3_pin, INPUT);
+  pinMode(button4_pin, INPUT);
+  pinMode(5, OUTPUT);
+
+  digitalWrite(16, LOW);
+  digitalWrite(5, LOW);
+
+//  delay(50); // This small delay is required for correct button detection
+
+  button = readButtons();
+
+  if (!SPIFFS.begin()) {
+    Serial.println("Failed to mount file system");
+  }
+
+  readConfig();
+
+  const char* ssid = json["ssid"].as<const char*>();
+  const char* pass = json["pass"].as<const char*>();
+  const char* ip = json["ip"].as<const char*>();
+  const char* gw = json["gw"].as<const char*>();
+  const char* sn = json["sn"].as<const char*>();
+
+  const char* m1 = json["m1"].as<const char*>();
+
+  if (ssid[0] != '\0' && pass[0] != '\0') {
+    WiFi.mode(WIFI_STA);
+
+    if (ip[0] != '\0' && gw[0] != '\0' && sn[0] != '\0') {
+      IPAddress ip_address, gateway_ip, subnet_mask;
+      if (!ip_address.fromString(ip) || !gateway_ip.fromString(gw) || !subnet_mask.fromString(sn)) {
+        Serial.println("Error setting up static IP, using auto IP instead. Check your configuration.");
+      } else {
+        WiFi.config(ip_address, gateway_ip, subnet_mask);
+      }
+    }
+
+    //serializeJson(json, Serial);
+
+    WiFi.begin(ssid, pass);
+
+    int iterator = 0;
+    while (WiFi.status() != WL_CONNECTED) {
+      iterator++;
+      if (iterator > 400) { // 4s timeout
+        deviceMode = CONFIG_MODE;
+        Serial.print("Failed to connect to: ");
+        Serial.println(ssid);
+        break;
+      }
+      delay(10);
+    }
+    /* */
+    Serial.println("Wifi connected...");
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+    Serial.print("Mac address: ");
+    Serial.println(WiFi.macAddress());
+    Serial.print("IP: ");
+    Serial.println(WiFi.localIP());
+
+    WiFi.macAddress(mac);
+
+    // MQTT SETUP
+    if (m1[0] != '\0') {
+      IPAddress broker_address;
+      if (!broker_address.fromString(m1)) {
+        Serial.println("Error setting up mqtt server IP. Check your configuration.");
+      } else {
+        client.setServer(broker_address, 1883);
+      }
+    } else {
+      Serial.println("mqtt server IP not set. Check your configuration.");
+    }
+
+  } else {
+    deviceMode = CONFIG_MODE;
+    Serial.println("No credentials set, going to config mode");
+    //startConfigPortal();
+    //goToSleep();
+  }
+
+  rst_info *rinfo;
+  rinfo = ESP.getResetInfoPtr();
+
+  String ota_name = "Hugh_" + macLastThreeSegments(mac);
+  ArduinoOTA.setHostname(ota_name.c_str());
+  ArduinoOTA.begin();
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("OTA UPLOAD STARTED...");
+    stopBlinking();
+    digitalWrite(5, HIGH);
+  });
+
+}
+
+void loop() {
+
+  if (deviceMode == OTA_MODE) {
+    Serial.println("WAITING FOR OTA UPDATE...");
+    startOTA();
+    Serial.println("RETURNING TO NORMAL MODE...");
+    return;
+  }
+
+  if (deviceMode == CONFIG_MODE) {
+    Serial.println("STARTING CONFIG ACCESS POINT, PRESS ANY BUTTON TO EXIT...");
+    startConfigPortal();
+    Serial.println("RETURNING TO NORMAL MODE...");
+    return;
+  }
+
+  if (deviceMode == CONFIG_MODE_LOCAL) {
+    Serial.println("STARTING LOCAL CONFIG MODE, PRESS ANY BUTTON TO EXIT...");
+    Serial.print("IP: ");
+    Serial.println(WiFi.localIP());
+    startLocalConfigPortal();
+    Serial.println("RETURNING TO NORMAL MODE...");
+    return;
+  }
+
+  toggleOTAMode();
+
+  toggleConfigMode();
+
+  toggleLocalConfigMode();
+
+  if (deviceMode != NORMAL_MODE) return;
+
+  mqtt_connect();
+
+  client.publish("esp/hugh/battery", String(batteryPercentage()).c_str());
+  Serial.println(String(batteryPercentage()));
+
+  if (button == 1) {
+    Serial.println("B1");
+    const char* b1t = json["b1t"].as<const char*>();
+    const char* b1p = json["b1p"].as<const char*>();
+    if (strlen(b1t) > 0 && strlen(b1p) > 0) {
+      client.publish(b1t, b1p);
+    } else {
+      Serial.println("Bad B1");
+    }
+  }
+  else if (button == 2) {
+    Serial.println("B2");
+    const char* b2t = json["b2t"].as<const char*>();
+    const char* b2p = json["b2p"].as<const char*>();
+    if (strlen(b2t) > 0 && strlen(b2p) > 0) {
+      client.publish(b2t, b2p);
+    } else {
+      Serial.println("Bad B2");
+    }
+  }
+  else if (button == 3) {
+    Serial.println("B3");
+    const char* b3t = json["b3t"].as<const char*>();
+    const char* b3p = json["b3p"].as<const char*>();
+    if (strlen(b3t) > 0 && strlen(b3p) > 0) {
+      client.publish(b3t, b3p);
+    } else {
+      Serial.println("Bad B3");
+    }
+  }
+  else if (button == 4) {
+    Serial.println("B4");
+    const char* b4t = json["b4t"].as<const char*>();
+    const char* b4p = json["b4p"].as<const char*>();
+    if (strlen(b4t) > 0 && strlen(b4p) > 0) {
+      client.publish(b4t, b4p);
+    } else {
+      Serial.println("Bad B4");
+    }
+  }
+
+  client.loop();
+  client.disconnect();
+
+  digitalWrite(5, HIGH);
+  delay(20);
+  digitalWrite(5, LOW);
+
+
+  //if (millis() - sleepMillis >= sleepDelay) {
+  goToSleep();
+  //}
+
+}
---
+++ Firmware/Hugh_MqttTrigger/fns.ino
@@ -0,0 +1,176 @@
+void toggleLed(int blinkSpeed) {
+  digitalWrite(5, ledState);
+  ledState = !ledState;
+
+  ticker.attach_ms(blinkSpeed, toggleLed, blinkSpeed);
+}
+
+void startBlinking(int blinkingSpeed) {
+  ticker.attach_ms(blinkingSpeed, toggleLed, blinkingSpeed);
+}
+
+void stopBlinking() {
+  ticker.detach();
+  digitalWrite(5, LOW);
+}
+
+void goToSleep() {
+  Serial.println("going to sleep");
+
+  /*
+      This should force all buttons to discharge
+      and allow for faster response time
+  */
+  /*
+    pinMode(button1_pin, OUTPUT);
+    pinMode(button2_pin, OUTPUT);
+    pinMode(button3_pin, OUTPUT);
+    pinMode(button4_pin, OUTPUT);
+    digitalWrite(button1_pin, LOW);
+    digitalWrite(button2_pin, LOW);
+    digitalWrite(button3_pin, LOW);
+    digitalWrite(button4_pin, LOW);
+  */
+
+  yield();
+  delay(5);
+  ESP.deepSleep(0);
+  yield();
+}
+
+String macToStr(const uint8_t* mac) {
+  String result;
+  for (int i = 0; i < 6; ++i) {
+    result += String(mac[i], 16);
+    if (i < 5)
+      result += ':';
+  }
+  return result;
+}
+
+String macLastThreeSegments(const uint8_t* mac) {
+  String result;
+  for (int i = 2; i < 6; ++i) {
+    result += String(mac[i], 16);
+  }
+  return result;
+}
+
+void mqtt_connect() {
+  //while (!client.connected()) {
+  Serial.print("Attempting MQTT connection...");
+  if (client.connect("esp/hugh")) {
+    client.publish("esp/hugh/battery", "" + batteryPercentage());
+    Serial.println("" + batteryPercentage());
+    Serial.println("connected");
+  } else {
+    Serial.print("failed, rc=");
+    Serial.print(client.state());
+    goToSleep();
+  }
+  //}
+}
+
+/*
+  void sendHttpRequest(String buttonUrl) {
+  int batteryPercent = batteryPercentage();
+  if (batteryPercent > 100) batteryPercent = 100;
+
+  buttonUrl.replace("[blvl]", (String)batteryPercent);
+  buttonUrl.replace("[mac]", macToStr(mac));
+
+  if (buttonUrl.length() == 0 || buttonUrl == "null" || buttonUrl == NULL) {
+    Serial.println("Button URL is not defined. Set it in config portal.");
+    return;
+  }
+  HTTPClient http;
+  http.begin(buttonUrl);
+
+  int httpCode = http.GET();
+
+  if (httpCode > 0) {
+    Serial.print("Successful request to URL: ");
+  } else {
+    Serial.print("Error connecting to URL: ");
+  }
+  http.end();   //Close connection
+  Serial.println(buttonUrl);
+
+  }
+*/
+int readButtons() {
+  if (digitalRead(button1_pin) == HIGH) {
+    return 1;
+  } else if (digitalRead(button2_pin) == HIGH) {
+    return 2;
+  } else if (digitalRead(button3_pin) == HIGH) {
+    return 3;
+  } else if (digitalRead(button4_pin) == HIGH) {
+    return 4;
+  }
+  return 0;
+}
+
+/* Read analog input for battery measurement */
+int ReadAIN()
+{
+  int Read = analogRead(A0);
+  int num = 10;
+
+  for (int i = 0 ; i < num ; i++)
+  {
+    int newRead = analogRead(A0);
+    if (newRead > Read)
+    {
+      Read = newRead;
+    }
+    delay(1);
+  }
+  return (Read);
+}
+
+/* Battery percentage estimation, this is not very accurate but close enough */
+uint8_t batteryPercentage() {
+  int analogValue = ReadAIN();
+  if (analogValue > 1000) return 200; // CHARGING
+  if (analogValue > 960) return 100;
+  if (analogValue > 940) return 90;
+  if (analogValue > 931) return 80;
+  if (analogValue > 922) return 70;
+  if (analogValue > 913) return 60; // 3.8v ... 920
+  if (analogValue > 904) return 50;
+  if (analogValue > 895) return 40;
+  if (analogValue > 886) return 30;
+  if (analogValue > 877) return 20; // 3.65v ... 880
+  if (analogValue > 868) return 10;
+  return 0;
+}
+
+bool readConfig() {
+  File stateFile = SPIFFS.open("/config.json", "r");
+  if (!stateFile) {
+    Serial.println("Failed to read config file... first run?");
+    Serial.println("Creating file and going to sleep. Try again!");
+    json["ssid"] = json["pass"] = json["ip"] = json["gw"] = json["sn"] = json["b1t"] = json["b2t"] = json["b3t"] = json["b4t"] = json["b1p"] = json["b2p"] = json["b3p"] = json["b4p"] = json["m1"] = json["m2"] = "";
+    saveConfig();
+    goToSleep();
+    return false;
+  }
+  DeserializationError error = deserializeJson(json, stateFile.readString());
+  stateFile.close();
+  //Serial.println("json:");
+  //serializeJson(json, Serial);
+  return true;
+}
+
+bool saveConfig() {
+  File configFile = SPIFFS.open("/config.json", "w");
+  if (!configFile) {
+    Serial.println("Failed to open config file for writing");
+    return false;
+  }
+  serializeJson(json, configFile);
+  //serializeJson(json, Serial);
+  configFile.close();
+  return true;
+}
